b06d5b18520d5a78c8689dd72d05aa7ed5459b49
Carlos Martínez <carlos.martinezg@estudiants.urv.cat>
2022-05-17 18:15:21 +0200

    CHANGES
    Co-Authored-By: nilm9 <85683608+nilm9@users.noreply.github.com>

--- PruebaSQL.sql
+++ PruebaSQL.sql
@@ -4,27 +4,34 @@ create database BDNilCarlos;
 use BDNilCarlos;
 
 create table armesBio(
-    nom varchar(20) not null,
+    nom varchar(20) PRIMARY KEY,
     fecha date,
     potencial int(1),
     zona varchar(20) not null,
-    laboratori varchar(20) not null,
-    constraint  pk_armesBio primary key (nom)
+    laboratori varchar(20) not null
 ) engine=innodb;
 create table employees (
-    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
-    branch_id BIGINT UNSIGNED,
-    dni VARCHAR(20) UNIQUE,
-    name VARCHAR(150),
-    email VARCHAR(100),
-    birth_date DATE,
-    created_at TIMESTAMP NULL,
-    updated_at TIMESTAMP NULL  
-)engine=innodb;
-insert into employees(id)values(123);
+    num_pass varchar(11),
+    nom varchar(150)
 
-insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma1','2022-05-24',7,'Vila-seca','Lab prova');
-insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma2','2022-03-21',7,'Guatemala','Lab guatemailano');
-insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma3','2022-05-1997',7,'Paraguai','Lab de paraguais');
-insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma4','2021-07-13',7,'CanyadaReal','Lab de coca');
+)engine=innodb;
+
+create table paisos(
+    nom varchar(20) PRIMARY KEY,
+    pot_desenc boolean,
+    tractat_signat boolean
+) engine=innodb;
+
+create table laboratoris(
+    codi int(20) PRIMARY KEY,
+    nom varchar(20),
+    paisos VARCHAR(20),
+    constraint  fk_paisos foreign key (nom) REFERENCES paisos(nom)
+
+) engine=innodb;
+
+
+
+
+
 
---
+++ insertaBD.sql
@@ -0,0 +1,7 @@
+use BDNilCarlos;
+insert into employees(num_pass,nom) values('12345678P','Nil');
+insert into employees(num_pass,nom) values('49424598J','Carlos');
+insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma1','2022-05-24',7,'Vila-seca','Lab prova');
+insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma2','2022-03-21',7,'Guatemala','Lab guatemailano');
+insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma3','2022-05-03',7,'Paraguai','Lab de paraguais');
+insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma4','2021-07-13',7,'CanyadaReal','Lab de coca');
