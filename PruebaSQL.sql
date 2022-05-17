8cb0b38a6ac6690675ebb0dc2cd12be8ad22748d
Carlos Martínez <76976573+carlosmgv02@users.noreply.github.com>
2022-05-17 15:50:45 +0200

    Add files via upload

---
+++ PruebaSQL.sql
@@ -0,0 +1,30 @@
+drop database BDNilCarlos;
+
+create database BDNilCarlos;
+use BDNilCarlos;
+
+create table armesBio(
+    nom varchar(20) not null,
+    fecha date(1),
+    potencial int(1),
+    zona varchar(20) not null,
+    laboratori varchar (20)not null,
+    constraint  pk_armesBio primary key (nom)
+) engine=innodb;
+CREATE TABLE employees (
+    id BIGINT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+    branch_id BIGINT UNSIGNED,
+    dni VARCHAR(20) UNIQUE,
+    name VARCHAR(150),
+    email VARCHAR(100),
+    birth_date DATE,
+    created_at TIMESTAMP NULL,
+    updated_at TIMESTAMP NULL  
+);
+insert into employees(id)values(123);
+
+-- insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma1','2022-05-24',7,'Vila-seca','Lab prova');
+-- insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma2','2022-03-21',7,'Guatemala','Lab guatemailano');
+-- insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma3','2022-05-1997',7,'Paraguai','Lab de paraguais');
+-- insert into armesBio(nom,fecha,potencial,zona,laboratori) values('Arma4','2021-07-13',7,'CanyadaReal','Lab de coca');
+
